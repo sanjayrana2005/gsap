2d058d04af460bb6473a3cba6802ce602b191ad4
Sanjay Rana <sanjayrana5113@gmail.com>
2026-02-01 20:12:42 +0530

    timeline refactoring

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
-gsap.to("#box1", {
-    x: 2000,
-    rotate: "45deg",
-    duration: 2.5,
-    delay:1,    
-});
-gsap.to("#box2", {
-    x: 2000,
-    rotate: "45deg",
-    duration: 2.5,
-    delay:3.5,    
-});
-gsap.to("#box3", {
-    x: 2000,
-    rotate: "45deg",
-    duration: 2.5,
-    delay:6,    
-});
+// gsap.to("#box1", {
+//     x: 2000,
+//     rotate: "45deg",
+//     duration: 2.5,
+//     delay:1,    
+// });
+// gsap.to("#box2", {
+//     x: 2000,
+//     rotate: "45deg",
+//     duration: 2.5,
+//     delay:3.5,    
+// });
+// gsap.to("#box3", {
+//     x: 2000,
+//     rotate: "45deg",
+//     duration: 2.5,
+//     delay:6,    
+// });
+
+var tl = gsap.timeline();
+tl.to("#box1",{
+    x:2000,
+    rotate:"45deg",
+    duration:2.5,
+    delay:1,
+})
+tl.to("#box2",{
+    x:2000,
+    rotate:"45deg",
+    duration:3,
+})
+tl.to("#box3",{
+    x:2000,
+    rotate:"45deg",
+    duration:2.5,
+})
