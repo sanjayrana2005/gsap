1bd904d1d5e9fdfcaf08c693873e82eb622b4fc6
Sanjay Rana <sanjayrana5113@gmail.com>
2026-02-03 00:25:44 +0530

    scrollTrigger

--- script.js
+++ script.js
@@ -1,32 +1,19 @@
-// gsap.to("#box1", {
-//     x: 2000,
-//     rotate: "45deg",
-//     duration: 2.5,
-//     delay:1,    
-// });
-// gsap.to("#box2", {
-//     x: 2000,
-//     rotate: "45deg",
-//     duration: 2.5,
-//     delay:3.5,    
-// });
-// gsap.to("#box3", {
-//     x: 2000,
-//     rotate: "45deg",
-//     duration: 2.5,
-//     delay:6,    
-// });
-
-var tl = gsap.timeline();
-gsap.to("#logo h1", {
-    y: 30,
-    opacity: 1,
-    duration: 1.5,
-    delay: 0.5,
-});
-gsap.to("#links h1", {
-    y: 30,
-    opacity: 1,
-    delay: 0.5,
-    stagger:1
-});
+gsap.from("#page2 h1",{
+    xPercent:-160,
+    duration:2,
+    scrollTrigger:{
+        trigger:"#page2",
+        scroller:"body",
+        start:"top 0%",
+        scrub:2,
+        pin:true
+    }
+})
+gsap.from("#page2 h2",{
+    xPercent:580,
+    scrollTrigger:{
+        trigger:"#page2",
+        start:"top 0%",
+        scrub:true
+    }
+})
